e19a215397bc1fc7bcf1cc07974afb99309c9681
Oleg Krechkovskiy <Olegkrechk@gmail.com>
2026-07-03 14:24:47 +0300

    добавил коммент

--- src/app/api/test-db/route.ts
+++ src/app/api/test-db/route.ts
@@ -4,6 +4,7 @@ import { NextResponse } from 'next/server';
 export async function GET() {
   try {
     const { db } = await connectToDatabase();
+    //Это отправляет MongoDB команду ping. Сервер отвечает { ok: 1 }. Если ответа нет — будет ошибка.
     await db.command({ ping: 1 });
     return NextResponse.json({
       status: 'connected',
